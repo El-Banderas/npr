975263c03d57544b34e4dc4791074439fa691100
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2023-03-12 18:42:41 +0000

    /tmp/

--- carros/src/Car/Main.java
+++ carros/src/Car/Main.java
@@ -92,8 +92,10 @@ public class Main {
 		else {
 			// Read node name
 			System.out.println("Working Directory = " + System.getProperty("user.dir"));
-			Pattern p = Pattern.compile("(\\/tmp\\/pycore\\.\\d+\\/)(\\w+)\\.conf");
+			//Pattern p = Pattern.compile("(\\/tmp\\/pycore\\.\\d+\\/)(\\w+)\\.conf");
+			Pattern p = Pattern.compile("\\/tmp\\/");
 			Matcher m = p.matcher(System.getProperty("user.dir"));
+			System.out.println("match = " + m.group());
 			String parent_dir = m.group(1);
 			String node_name = m.group(2);
 			
