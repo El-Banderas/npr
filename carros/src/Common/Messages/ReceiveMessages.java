3f495d035e77e9e6e28ed7d6ca55ee69082bcfd7
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2023-03-22 15:38:13 +0000

    MessageAndType a guardar o pacote sem "header", em vez do pacote todo (vou testar)

--- carros/src/Common/Messages/ReceiveMessages.java
+++ carros/src/Common/Messages/ReceiveMessages.java
@@ -9,12 +9,19 @@ import java.nio.ByteBuffer;
 public class ReceiveMessages {
 
 
-	public static MessageAndType receiveData(DatagramSocket socket) throws IOException {
+	public static MessageAndType receiveData(DatagramSocket socket) throws IOException
+	{
 		byte[] buf = new byte[MessagesConstants.sizeBufferMessages];
 		DatagramPacket packet = new DatagramPacket(buf, buf.length);
 		socket.receive(packet);
-		int type = ByteBuffer.wrap(buf).getInt();
-		MessageAndType received = new MessageAndType(type, packet.getData(), packet.getAddress());
+		
+		ByteBuffer bbuf = ByteBuffer.wrap(buf);
+		int type = bbuf.getInt();
+		
+		byte[] remaining = new byte[bbuf.remaining()];
+		bbuf.get(remaining, 0 /*bbuf.position()*/, bbuf.remaining());
+		
+		MessageAndType received = new MessageAndType(type, remaining, packet.getAddress());
 		return received;
 	}
 }
--- carros/src/Common/Messages/SendMessages.java
+++ carros/src/Common/Messages/SendMessages.java
@@ -14,79 +14,110 @@ import Car.CarInfo;
 
 public class SendMessages {
 
-	public static void carSendBreak(DatagramSocket sender){
-		if (Constants.core){
+	public static void carSendBreak(DatagramSocket sender)
+	{
+		if(Constants.core) {
 			System.out.println("Send Break to everyone");
-			byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages).putInt(MessagesConstants.BreakMessage).array();
-			DatagramPacket packet = new DatagramPacket(buf, buf.length, Constants.MulticastGroup, Constants.portMulticast);
-			sendMessage(sender, packet);
-		}
-	}
-
-	public static void carHellos(DatagramSocket sender, CarInfo info){
-		if (Constants.core){
-			//System.out.println("Send hellos to everyone");
+			
 			byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages)
-					.putInt(MessagesConstants.CarHelloMessage)
-					.put(info.id.getBytes())
-					.put("Hello".getBytes())
+					.putInt(MessagesConstants.BreakMessage)
 					.array();
+			
 			DatagramPacket packet = new DatagramPacket(buf, buf.length, Constants.MulticastGroup, Constants.portMulticast);
 			sendMessage(sender, packet);
 		}
 	}
 	
-	public static void carHelloTower(DatagramSocket sender, InfoNode destination){
-		byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages).putInt(MessagesConstants.CarHelloMessage).put("Hello".getBytes()).array();
-		DatagramPacket packet;
-		if (Constants.core){
-			packet = new DatagramPacket(buf, buf.length, Constants.MulticastGroup, Constants.portMulticast);
+	public static void carHellos(DatagramSocket sender, CarInfo info)
+	{
+		if(Constants.core) {
+			//System.out.println("Send hellos to everyone");
+			
+			byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages)
+					.putInt(MessagesConstants.CarHelloMessage)
+					.put(info.id.getBytes())
+					.put("Hello".getBytes())
+					.array();
+			
+			DatagramPacket packet = new DatagramPacket(buf, buf.length, Constants.MulticastGroup, Constants.portMulticast);
+			sendMessage(sender, packet);
 		}
-		else {
+	}
+	
+	public static void carHelloTower(DatagramSocket sender, InfoNode destination)
+	{
+		byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages)
+				.putInt(MessagesConstants.CarHelloMessage)
+				.put("Hello".getBytes())
+				.array();
+		
+		DatagramPacket packet;
+		if(Constants.core) {
+			packet = new DatagramPacket(buf, buf.length, Constants.MulticastGroup, Constants.portMulticast);
+		} else {
 			packet = new DatagramPacket(buf, buf.length, destination.ip, destination.port);
 		}
 		sendMessage(sender, packet);
 	}
-
-	public static void towerHelloServer(DatagramSocket sender, InfoNode destination){
-		byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages).putInt(MessagesConstants.TowerHelloMessage).put("Hello".getBytes()).array();
+	
+	public static void towerHelloServer(DatagramSocket sender, InfoNode destination)
+	{
+		byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages)
+				.putInt(MessagesConstants.TowerHelloMessage)
+				.put("Hello".getBytes())
+				.array();
 
 		DatagramPacket packet = new DatagramPacket(buf, buf.length, destination.ip, destination.port);
 		sendMessage(sender, packet);
 	}
-
-	public static void serverHelloCloud(DatagramSocket sender){
-		byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages).putInt(MessagesConstants.ServerHelloMessage).put("Hello".getBytes()).array();
+	
+	public static void serverHelloCloud(DatagramSocket sender)
+	{
+		byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages)
+				.putInt(MessagesConstants.ServerHelloMessage)
+				.put("Hello".getBytes())
+				.array();
 
 		DatagramPacket packet = new DatagramPacket(buf, buf.length, CloudConstants.ip, CloudConstants.port);
 		sendMessage(sender, packet);
 	}
-
-
-
-	public static void carSendAccident(DatagramSocket send) {
-		if (Constants.core){
+	
+	public static void carSendAccident(DatagramSocket send)
+	{
+		if(Constants.core) {
 			System.out.println("!!!!Send accident to everyone");
-			byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages).putInt(MessagesConstants.AccidentMessage).array();
+			
+			byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages)
+					.putInt(MessagesConstants.AccidentMessage)
+					.array();
+			
 			DatagramPacket packet = new DatagramPacket(buf, buf.length, Constants.MulticastGroup, Constants.portMulticast);
 			sendMessage(send, packet);
 		}
 	}
-	public static void forwardMessage(MessageAndType message, DatagramSocket sendSocket, InfoNode thisServer) {
+	
+	public static void forwardMessage(MessageAndType message, DatagramSocket sendSocket, InfoNode thisServer)
+	{
 		DatagramPacket packet = new DatagramPacket(message.content, message.content.length, thisServer.ip, thisServer.port);
 		sendMessage(sendSocket, packet);
 	}
-
-	public static void towerHelloCar(DatagramSocket sendSocket) {
-		if (Constants.core){
+	
+	public static void towerHelloCar(DatagramSocket sendSocket)
+	{
+		if(Constants.core) {
 			System.out.println("Send accident to everyone");
-			byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages).putInt(MessagesConstants.TowerHelloMessage).array();
+			
+			byte[] buf = ByteBuffer.allocate(MessagesConstants.sizeBufferMessages)
+					.putInt(MessagesConstants.TowerHelloMessage)
+					.array();
+			
 			DatagramPacket packet = new DatagramPacket(buf, buf.length, Constants.MulticastGroup, Constants.portMulticast);
 			sendMessage(sendSocket, packet);
 		}
 	}
-
-	public static void sendMessage(DatagramSocket send, DatagramPacket packet){
+	
+	public static void sendMessage(DatagramSocket send, DatagramPacket packet)
+	{
 		try {
 			send.send(packet);
 		} catch (IOException e) {
@@ -94,5 +125,4 @@ public class SendMessages {
 			System.out.println("[ERROR] in sending message");
 		}
 	}
-
 }
