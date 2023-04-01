7dfb102c493e6883388464623b08683fa8fadc09
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2023-04-01 16:27:22 +0100

    Tirar printezinha

--- carros/src/Common/FWRMessages/FWReceiveMessages.java
+++ carros/src/Common/FWRMessages/FWReceiveMessages.java
@@ -29,10 +29,8 @@ public class FWReceiveMessages {
         byte[] buf = new byte[MessagesConstants.sizeBufferMessages];
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
         receiveSocket.receive(packet);
-        if (packet.getAddress().equals(myIp)) {
-            System.out.println("Aposto que vem para aqui...");
+        if (packet.getAddress().equals(myIp)) 
             throw new IOException();
-        }
         FWRInfo fwrinfo = new FWRInfo(buf);
 
 
