fa9d2c3c0e964b9915564b097aa4f1bdfb1b9cf0
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2023-04-04 14:22:08 +0100

    testettt

--- carros/src/AWFullP/ReceiveMessages.java
+++ carros/src/AWFullP/ReceiveMessages.java
@@ -47,7 +47,7 @@ public class ReceiveMessages
 		AWFullPacket aw = new AWFullPacket(buf);
 
 		String before = aw.toString();
-		System.out.println("Recebeu mensagem de: " + packet.getAddress());
+		System.out.println("Recebeu mensagem de próprio: "+packet.getAddress().equals(myIp) + " Pacote: " + packet.getAddress() + " EU:  " + myIp.toString());
 		if (packet.getAddress().equals(myIp) || aw.forwardInfo.getSenderID().equals(myID)) {
 			if (debug) logger.info("\nPossible self message (me: " + myIp + ", other: " + packet.getAddress() + ", message: " + before + ")\n");
 			throw new SelfCarMessage();
